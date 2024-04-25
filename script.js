09d185d16ea8d8bdb9546a1290d223b3c329590f
Elena Lem <151932734+Leksa2304@users.noreply.github.com>
2024-04-25 15:37:01 +0300

    hometask-9. Продвинутый уровень
    откорректированы некоторые переменные, которые были в выборке со свойством textContent. Функция stopTimer оптимизировала код

--- script.js
+++ script.js
@@ -1,56 +1,51 @@
 const startStopButton = document.querySelector("#start");
 const resetButton = document.querySelector("#reset");
-
-const setTimer = document.querySelector("#pomodoro-time").textContent;
-
+const breakButton = document.querySelector("#break");
+const pomodoroButton = document.querySelector("#pomodoro");
 const time = document.querySelector("#pomodoro-time");
 
-
-
 let timerId;
-let timerStoped = true;
+let isRunning = false;
+
+function stopTimer() {
+
+    isRunning = false;
+    startStopButton.textContent = "start";
+    clearTimeout(timerId);
+};
+
 
 function startStopTimer() {
 
-    if (timerStoped) {
-        timerStoped = false;
+    if (isRunning) {
+        stopTimer();
+        return;
+    };
 
-        startStopButton.textContent == "start"
+    timerId = setInterval(() => {
+        let [minutes, seconds] = time.textContent.split(":").map(Number);
 
-        timerId = setInterval(() => {
-            let [minutes, seconds] = time.textContent.split(":").map(Number);
+        if (seconds > 0) {
+            seconds -= 1;
+        } else if (minutes > 0) {
+            minutes -= 1;
+            seconds = 59;
+        }
 
-            if (seconds > 0) {
-                seconds -= 1;
-            } else if (minutes > 0) {
-                minutes -= 1;
-                seconds = 59;
-            }
+        if (seconds >= 0 && minutes >= 0) {
+            time.textContent = `${format(minutes)}:${format(seconds)}`
+        }
 
-            if (seconds >= 0 && minutes >= 0) {
-                time.textContent = `${format(minutes)}:${format(seconds)}`
-            }
+        if (minutes === 0 && seconds === 0) {
+            clearInterval(timerId);
+            startStopButton.textContent = "start";
+            time.textContent = `${format(minutes)}:${format(seconds)}`
+        }
+    }, 10);
 
-            if (minutes === 0 && seconds === 0) {
-                clearInterval(timerId);
-                startStopButton.textContent = "start";
-                time.textContent = setTimer;
-
-
-            }
-        }, 10);
-
-        startStopButton.textContent = "stop"
-
-    } else {
-        timerStoped = true;
-        startStopButton.textContent = "start";
-        clearInterval(timerId);
-
-    }
-
+    startStopButton.textContent = "stop";
+    isRunning = !isRunning;
 }
-
 
 //добавление незначащих нулей
 function format(val) {
@@ -61,3 +56,29 @@ function format(val) {
 }
 
 startStopButton.addEventListener("click", startStopTimer);
+
+breakButton.addEventListener("click", function() {
+    stopTimer();
+    startStopButton.textContent = "start"
+    time.textContent = "05:00";
+    pomodoroButton.classList.remove("active");
+    breakButton.classList.add("active");
+
+});
+
+pomodoroButton.addEventListener("click", function() {
+    stopTimer();
+    time.textContent = "25:00";
+    pomodoroButton.classList.add("active");
+    breakButton.classList.remove("active");
+});
+
+resetButton.addEventListener("click", function() {
+    stopTimer();
+    if (pomodoroButton.classList.contains("active")) {
+        time.textContent = "25:00";
+    } else {
+        time.textContent = "05:00";
+    }
+
+});
